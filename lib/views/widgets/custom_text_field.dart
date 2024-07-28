308008b12b785f895f2f72107b6d57682e16f58d
MohamedHamedAbdulhakam <mohamedhamedabdulhakam@gmail.com>
2024-07-28 23:56:56 +0300

    custom text field part four

--- lib/views/widgets/custom_text_field.dart
+++ lib/views/widgets/custom_text_field.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:notes_app/constants.dart';
 
 class CustomTextField extends StatelessWidget {
   const CustomTextField({super.key});
@@ -6,15 +7,20 @@ class CustomTextField extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return TextField(
+      cursorColor: kprimarycolor,
       decoration: InputDecoration(
+        hintText: 'text',
+        hintStyle: TextStyle(color: kprimarycolor),
         border: build_method(),
+        enabledBorder: build_method(),
+        focusedBorder: build_method(kprimarycolor),
       ),
     );
   }
 
-  OutlineInputBorder build_method() {
+  OutlineInputBorder build_method([Color]) {
     return OutlineInputBorder(
-      borderRadius: BorderRadius.circular(8),
-    );
+        borderRadius: BorderRadius.circular(8),
+        borderSide: BorderSide(color: Color ?? Colors.white));
   }
 }
