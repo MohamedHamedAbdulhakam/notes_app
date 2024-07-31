99f8caaad88d64fc7e6daeebaf6819733b9fed68
MohamedHamedAbdulhakam <mohamedhamedabdulhakam@gmail.com>
2024-07-31 16:41:58 +0300

    create notes view model

---
+++ lib/models/note_model.dart
@@ -0,0 +1,12 @@
+class NoteModel {
+  final String Title;
+  final String SubTitle;
+  final String Date;
+  final int color;
+
+  NoteModel(
+      {required this.Title,
+      required this.SubTitle,
+      required this.Date,
+      required this.color});
+}
