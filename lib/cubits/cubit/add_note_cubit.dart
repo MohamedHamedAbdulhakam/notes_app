9d1103644599931a219583eb78e0cc9eb247020d
MohamedHamedAbdulhakam <mohamedhamedabdulhakam@gmail.com>
2024-08-06 23:10:25 +0300

    create note cubit

--- lib/cubits/cubit/add_note_cubit.dart
+++ lib/cubits/cubit/add_note_cubit.dart
@@ -9,6 +9,7 @@ part 'add_note_state.dart';
 class AddNoteCubit extends Cubit<AddNoteState> {
   AddNoteCubit() : super(AddNoteInitial());
   AddNote(NoteModel note) async {
+    emit(AddNoteLoading());
     try {
       var notebox = Hive.box<NoteModel>(kNoteBox);
 
--- lib/cubits/cubit/cubit/notes_cubit.dart
+++ lib/cubits/cubit/cubit/notes_cubit.dart
@@ -1,8 +1,25 @@
+import 'dart:ffi';
+
 import 'package:bloc/bloc.dart';
+import 'package:hive/hive.dart';
 import 'package:meta/meta.dart';
+import 'package:notes_app/constants.dart';
+
+import 'package:notes_app/models/note_model.dart';
 
 part 'notes_state.dart';
 
 class NotesCubit extends Cubit<NotesState> {
   NotesCubit() : super(NotesInitial());
+  fetchAllNotes() async {
+    emit(NotesLoading());
+    try {
+      var notebox = Hive.box<NoteModel>(kNoteBox);
+
+      List<NoteModel> notes = notebox.values.toList();
+      emit(NotesSuccess(notes));
+    } catch (e) {
+      emit(NotesFailure(e.toString()));
+    }
+  }
 }
